ffde682a63f07592a144337323328babc7b666f9
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-03-15 09:34:26 +0900

    設定ファイル読み込み

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 "use strict";
 
+const fs = require('fs');
 const Koa = require('koa');
+
+const config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+console.log(config)
 const app = new Koa();
 
 app.use(require('./router')());
