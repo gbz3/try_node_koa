a667ec0489fc4cae85ab74f0dbbded1ef745f485
gbz3 <1808483+gbz3@users.noreply.github.com>
2020-03-15 13:28:26 +0900

    配列から検索

--- router.js
+++ router.js
@@ -9,13 +9,13 @@ module.exports = function(config) {
         ctx.body = 'This is root.'
     })
     .get('/user', (ctx, next) => {
-        ctx.body = `This is ${config.user}.`
+        ctx.body = `This is ${config.users.find(o => o.username === 'user').username}.`
     })
     .get('/staff', (ctx, next) => {
-        ctx.body = `This is ${config.staff}.`
+        ctx.body = `This is ${config.users.find(o => o.username === 'staff').username}.`
     })
     .get('/admin', (ctx, next) => {
-        ctx.body = `This is ${config.admin}.`
+        ctx.body = `This is ${config.users.find(o => o.username === 'admin').username}.`
     })
 
   return router.routes();
